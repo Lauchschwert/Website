40a890281a74052b1f0d38f63ebfa2884ac95e1d
Andreas <anditv21@icloud.com>
2022-12-26 03:37:46 +0100

    ✅ Added copy dc tag onclick

--- assets/main.js
+++ assets/main.js
@@ -48,6 +48,16 @@ function deletetitle(txt, txt2) {
 
 }
 
+function setClipboard(value) {
+    var tempInput = document.createElement("input");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+}
+
 function cBgButton(lightmode, darkmode) {
 
 }
