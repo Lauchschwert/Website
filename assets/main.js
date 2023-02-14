910777668a9e4729d91c3c6d4fa3d85d3959011d
Andreas <anditv21@icloud.com>
2023-02-14 20:30:09 +0100

    🔧 Updated clock
    the clock is now displayed in 12h format

--- assets/main.js
+++ assets/main.js
@@ -3,11 +3,11 @@ let a = 0;
 
 function writeTitle(txt) {
   if (a === 0) {
-    document.getElementsByTagName('title')[0].innerHTML = txt.charAt(0);
+    document.getElementsByTagName("title")[0].innerHTML = txt.charAt(0);
     a = 1;
   }
   if (i < txt.length) {
-    document.getElementsByTagName('title')[0].innerHTML += txt.charAt(i);
+    document.getElementsByTagName("title")[0].innerHTML += txt.charAt(i);
     i++;
     setTimeout(() => {
       writeTitle(txt);
@@ -25,14 +25,14 @@ function deleteTitle(txt, txt2) {
   if (i > 1) {
     i--;
     txt = txt.slice(0, -1);
-    document.getElementsByTagName('title')[0].innerHTML = txt;
+    document.getElementsByTagName("title")[0].innerHTML = txt;
     setTimeout(() => {
       deleteTitle(txt, txt2);
     }, 200);
   } else {
     i = 1;
     a = 0;
-    document.getElementsByTagName('title')[0].innerHTML = txt.charAt(0);
+    document.getElementsByTagName("title")[0].innerHTML = txt.charAt(0);
     setTimeout(() => {
       writeTitle(txt2);
     }, 200);
@@ -40,38 +40,38 @@ function deleteTitle(txt, txt2) {
 }
 
 function setClipboard(value) {
-  const tempInput = document.createElement('input');
-  tempInput.style = 'position: absolute; left: -1000px; top: -1000px';
+  const tempInput = document.createElement("input");
+  tempInput.style = "position: absolute; left: -1000px; top: -1000px";
   tempInput.value = value;
   document.body.appendChild(tempInput);
   tempInput.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(tempInput);
 }
 
 function cBgButton(lightmode, darkmode) {
   // TODO: Add implementation for cBgButton function
 }
+
 function update_clock() {
   var currentTime = new Date(),
-      hours = currentTime.getHours(),
-      minutes = currentTime.getMinutes();
-  seconds = currentTime.getSeconds();
+    hours = currentTime.getHours(),
+    minutes = currentTime.getMinutes(),
+    seconds = currentTime.getSeconds();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+
   if (minutes < 10) {
-      minutes = "0" + minutes;
+    minutes = "0" + minutes;
   }
   if (seconds < 10) {
-      seconds = "0" + seconds;
-  }
-  if (hours < 12) {
-    minutes = minutes + " am";
-  }
-  if (hours > 12) {
-    minutes = minutes + " pm";
+    seconds = "0" + seconds;
   }
 
   var clock_element = document.getElementById("clock");
 
-  clock_element.innerHTML = hours + " : " + minutes;
+  clock_element.innerHTML = hours + " : " + minutes + " " + ampm;
 }
+
 setInterval(update_clock, 1000);
