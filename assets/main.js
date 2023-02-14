c6767702cb996e2138bf311b9ea625691a3f9e39
Niclas <niclas.arnoldner@gmail.com>
2023-02-14 05:08:17 +0100

    Changes
    💚 - added clock

--- assets/main.js
+++ assets/main.js
@@ -52,3 +52,20 @@ function setClipboard(value) {
 function cBgButton(lightmode, darkmode) {
   // TODO: Add implementation for cBgButton function
 }
+function update_clock() {
+  var currentTime = new Date(),
+      hours = currentTime.getHours(),
+      minutes = currentTime.getMinutes();
+  seconds = currentTime.getSeconds();
+  if (minutes < 10) {
+      minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+      seconds = "0" + seconds;
+  }
+
+  var clock_element = document.getElementById("clock");
+
+  clock_element.innerHTML = hours + " : " + minutes;
+}
+setInterval(update_clock, 1000);
